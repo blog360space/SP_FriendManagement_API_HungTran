2a97a18da91ac6a74de85affdcf14e5d98a2714f
hungtranm <mhungou04@gmail.com>
2020-04-10 15:34:40 +0700

    General: Update Respond format

--- src/utils/common.go
+++ src/utils/common.go
@@ -6,7 +6,11 @@ import (
 )
 
 func Message(status bool, message string) (map[string]interface{}) {
-	return map[string]interface{} {"status" : status, "message" : message}
+	if (status == true) {
+		return map[string]interface{} {"success" : status}
+	} else {
+		return map[string]interface{} {"success" : status, "message" : message}
+	}
 }
 
 func Respond(w http.ResponseWriter, data map[string] interface{})  {
